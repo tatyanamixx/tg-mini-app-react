154238ed377ac718ef8f122c91c62710479f920a
tatyanaMixx <tatyanamixx@gmal.com>
2025-04-24 10:25:01 +0500

    viewport

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import { viewport, init, isTMA } from '@telegram-apps/sdk';
 
 import { useTelegram } from './hooks/useTelegram';
 
@@ -12,8 +13,26 @@ function App() {
 		tg.ready();
 	});
 
+	useEffect(() => {
+		async function initTg() {
+			if (await isTMA()) {
+				init();
+
+				if (viewport.mount.isAvailable()) {
+					await viewport.mount();
+					viewport.expand();
+				}
+
+				if (viewport.requestFullscreen.isAvailable()) {
+					await viewport.requestFullscreen();
+				}
+			}
+		}
+		initTg();
+	}, []);
+
 	return (
-		<div>
+		<div className='wrapper'>
 			<Header />
 			{/* <Routes>
 				<Route index element={<ProductList />} />
